ac40a0dbbd6fd61de377fa8d372c7f3feeb4363f
Oumar Yanni <oumaryanni@gmail.com>
2023-08-27 23:16:43 +0000

    Using assets with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const {CleanWebpackPlugin } = require("clean-webpack-plugin");
 const portfinder = require('portfinder');
 
 const config = {
   entry: {
     main: path.join(__dirname, "src/index.js"),
     form: path.join(__dirname, "src/form/form.js"),
+    topbar: path.join(__dirname, "src/assets/javascripts/topbar.js")
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -25,15 +28,24 @@ const config = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: './src/assets/images/*',
+                to: 'assets/images/[name][ext]',
+            }
+        ]
+    }),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "./src/index.html"),
-      chunks: ["main"]
+      chunks: ["main", "topbar"]
     }),
     new HtmlWebpackPlugin({
         filename: "form.html",
         template: path.join(__dirname, "./src/form/form.html"),
-        chunks: ["form"]
+        chunks: ["form", "topbar"]
     })
   ],
   stats: "minimal",
