2d50e84ae6c5ae5d60243a266bc1b641516c4e21
Oumar Yanni <oumaryanni@gmail.com>
2023-10-06 13:11:16 +0000

    Setting up article editing

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ const createArticles = articles => {
         </p>
         <div class="article-actions">
          <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+         <button class="btn btn-primary" data-id=${article._id}>Modifier</button>
          </div>
         `;
          return articleDOM
@@ -40,6 +41,17 @@ const createArticles = articles => {
 
     
     const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+
+    const editButtons = articleContainerElement.querySelectorAll(".btn-primary");
+
+    editButtons.forEach(button => {
+        button.addEventListener("click", event => {
+            const target = event.target;
+            const articleId = target.dataset.id;
+            window.location.assign(`/form.html?id=${articleId}`);
+        });
+    });
+
     deleteButtons.forEach(button => {
         button.addEventListener("click", async event => {
             try {
